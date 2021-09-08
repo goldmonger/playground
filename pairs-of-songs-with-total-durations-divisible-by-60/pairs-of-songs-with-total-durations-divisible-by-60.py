1e0dfff8561757b84548f15ef56b3ec3b27eac86
acetone7 <raunakhebballi@gmail.com>
2021-09-08 17:44:18 +0530

    Time: 340 ms (14.07%), Space: 17.5 MB (92.85%) - LeetHub

---
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        #print(time)
+        remList = [0] * 60
+        counter = 0
+        for i in range(len(time)):
+            # in this loop we are iterating over the aray and finding out the mod
+            # result and storing into a remainder array at the remainders index
+            # in increments
+            # ex if remainder is 10
+            # rem[10] += 1
+            
+            rem = time[i] % 60
+            if(rem == 0):
+                #print(rem, " a is a multiple of 60")
+                # after the second pass all the zero elements need to be tracked
+                counter += remList[0]
+            else:
+                #print(rem, " a is not a multiple, b: ", 60-rem)
+                # check if 60-rem has any count of encountered
+                # remainders
+                counter += remList[60 - rem]
+            #print(rem)
+            
+            remList[rem] += 1
+        #print (remList)
+        return (counter)
